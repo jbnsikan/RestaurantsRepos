6ca930c2473b580befdd7669cc9d31a55afb1073
jbnsi <jbnsi@LAPTOP-7SE6IRJP>
2025-09-24 21:24:09 +0200

    Authentication

---
+++ Restaurants.Application/Users/UserContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+
+namespace Restaurants.Application.Users
+{
+    public class UserContext(IHttpContextAccessor httpContextAccessor) : IUserContext
+    {
+        public CurrentUser? GetCurrentUser()
+        {
+            var user = (httpContextAccessor?.HttpContext?.User) ?? throw new InvalidOperationException("User context is not present");
+            if (user?.Identity == null || !user.Identity.IsAuthenticated)
+            {
+                return null;
+            }
+
+            var userid = user.FindFirst(c => c.Type == ClaimTypes.NameIdentifier)!.Value;
+            var email = user.FindFirst(c => c.Type == ClaimTypes.Email)!.Value;
+            var roles = user.Claims.Where(c => c.Type == ClaimTypes.Role)!.Select(c => c.Value);
+
+            return new CurrentUser(userid, email, roles);
+        }
+    }
+
+    public interface IUserContext
+    {
+        CurrentUser? GetCurrentUser();
+    }
+}
---
+++ Restaurants/Extensions/WebApiBuilderExtensions.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.OpenApi.Models;
+using Restaurants.API.Middlewares;
+using Serilog;
+
+namespace Restaurants.API.Extensions
+{
+    public static class WebApiBuilderExtensions
+    {
+        public static void AddPresentation(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddAuthentication();
+            builder.Services.AddControllers();
+
+            builder.Services.AddScoped<ErrorHandlingMiddle>();
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.AddSecurityDefinition("bearerAuth", new OpenApiSecurityScheme
+                {
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "Bearer"
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference{ Type = ReferenceType.SecurityScheme, Id = "bearerAuth"}
+                        },
+                        []
+                    }
+                });
+            });
+
+            builder.Host.UseSerilog((context, configuration)
+               => configuration
+               .ReadFrom.Configuration(context.Configuration)
+               );
+        }
+    }
+}
