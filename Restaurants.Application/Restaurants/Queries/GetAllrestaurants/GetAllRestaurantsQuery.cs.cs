d52c0ef8c0e70cca2c08653e474487e7036e8bb6
jbnsi <jbnsi@LAPTOP-7SE6IRJP>
2025-10-04 13:52:55 +0200

    Result filtering server

--- Restaurants.Application/Restaurants/Queries/GetAllrestaurants/GetAllRestaurantsQuery.cs.cs
+++ Restaurants.Application/Restaurants/Queries/GetAllrestaurants/GetAllRestaurantsQuery.cs.cs
@@ -1,10 +1,14 @@
 ﻿using MediatR;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Restaurants.Application.Common;
 using Restaurants.Application.Restaurants.Dtos;
 
 namespace Restaurants.Application.Restaurants.Queries.GetAllrestaurants
 {
-    public class GetAllRestaurantsQuery : IRequest<IEnumerable<RestaurantDto>>
+    public class GetAllRestaurantsQuery : IRequest<PagedResult<RestaurantDto>>
     {
         public string? SearchPhrase { get; set; }
+        public int PageSize { get; set; }
+        public int PageNumber { get; set; }
     }
 }
